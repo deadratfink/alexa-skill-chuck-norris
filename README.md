054fce8984c364b8d9bcc265cae202f6250a66f8
Jens Krefeldt <jens.krefeldt@finanzcheck.de>
2017-09-06 03:24:16 +0200

    I18N, more optimized, translation to de with MS translator API

--- README.md
+++ README.md
@@ -1,11 +1,118 @@
 # alexa-skill-chuck-norris
 
-Alexa skill which integrates a Chuck Norris API (api.chucknorris.io).
+Alexa Lambda skill which integrates the [Chuck Norris API](https://api.chucknorris.io) utilizing the
+[alexa-sdk](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs).
 
-# Useful Alexa Links
+## Use Cases
+
+The skill supports the following use cases: 
+
+1. Ask for a random english joke/fact about Chuck Norris by calling the Chuck Norris API
+   (https://api.chucknorris.io/jokes/random). The result of the API call is passed to Alexa for reading.
+   
+   > Example: _Ask Chuck Norris a joke._
+2. When asking Alexa for a joke you can choose also a certain _category_ of jokes. These are defined by the API
+   and can be retrieved by a call to https://api.chucknorris.io/jokes/categories.
+   
+  |en-US, en-GB|
+  |---|
+  |explicit|
+  |dev|
+  |movie|
+  |food|
+  |celebrity|
+  |science|
+  |political|
+  |sport|
+  |religion|
+  |animal|
+  |music|
+  |history|
+  |travel|
+  |career|
+  |money|
+  |fashion|
+
+  > Example: _Ask Chuck Norris a_ `{category}` _joke._
+
+All this works fine for Alexa Devices which are configured to English language. For German language (and potentially others)
+there is a translation hook depending on the passed _locale_.
+
+### API Result Translation
+
+Alexa passes the _locale_ to the Lambda function. Any other language parsed from this _locale_ than `'en'` is
+forwarded to the Microsoft Translation API (alongside the result from _Chuck Norris API_) which returns the proper
+translation. Of course, due to the fact that automatic translation engines are not always perfect in doing their job 
+there could be strange or even funny results for the jokes/facts in other languages.
+
+The [Microsoft Translation API](https://www.microsoft.com/en-us/translator/translatorapi.aspx) is
+for _commercial use_ but provides an initial trial period. Anyway, you have to
+[register](https://www.microsoft.com/en-us/translator/getstarted.aspx) 
+first (make sure you have your credit card at hand) to use this service (read all MS instructions carefully
+while you create the translation account in the Azure Cloud). At the end of the setup you will obtain an _authorization key_
+which needs to be send on every request to the API. They provide an OAuth handshake which
+is not very handy in our case - but fortunately, there is also an API-key like authorization possibility where the
+_static_ authorization key can be sent via a special header named `Ocp-Apim-Subscription-Key`.
+
+Therefore, the Lambda function needs to be configured with an environment variable `MS_OCP_APIM_SUBSCRIPTION_KEY` where 
+you put your freshly obtained key (this is needed only if language with locales other than `en-US` and `en-GB` are
+configured, see also setup steps below).
+
+## General Setup
+
+First you need to setup a Lambda function, then create the skill and configure the Application ID.
+
+### Setup of Lambda Function
+
+1. Login with your account to [AWS Management Console](https://aws.amazon.com).
+2. Switch to _Lamda_ section.
+3. Click button _Create function_ (on the right top).
+4. Choose button _Author from scratch_.
+5. In _Configure triggers_ click on the dotted square (in _Add Trigger_ field) and choose _Alexa Skills Kit_, click _Next_ button.
+6. Copy the code from _./src/index.js_ to the _Lambda function code_'s field.
+7. Optional: If translation needed add `MS_OCP_APIM_SUBSCRIPTION_KEY` environment variable.
+8. Choose _lambda_basic_execution_ in _Existing role<sup>*</sup>_ field.
+9. Export the function.
+
+### Setup Alexa Skill
+
+1. Login to your [Amazon Developer Account](http://developer.amazon.com).
+2. Choose _Alexa_ tab.
+3. Click button _Add a New Skill_ (on the right top).
+4. In _Skill Information_:
+    1. Choose a _Name_, e.g. _Chuck Norris Jokes_.
+    2. Choose an _Invocation Name_, e.g. _chuck norris_.
+    3. Save.
+5. Switch to _Interaction Model_:
+    1. Insert _Intent Schema_ (copy from _./speechAssets/IntentSchema.json_).
+    2. Define _Custom Slot Type_ named `FACT_TYPE` and insert list values from _./speechAssets/slots.txt_.
+    3. Add _Sample Utterances_ (copy from _./speechAssets/SampleUtterances.txt_).
+6. In _Configuration_ insert the [ARN](https://docs.aws.amazon.com/en_us/general/latest/gr/aws-arns-and-namespaces.html)
+   of the created Lambda function into the _Default_ field.
+7. Optional: add more languages (of course, this requires the translation of the _utterances_ and fact types to the target language).
+
+### Configure the Application ID
+
+Once the skill is created you can find this value on the skill's page on http://developer.amazon.com. Copy it and
+configure the Lambda function with an environment variable `APP_ID` and assign the copied value.
+
+## Limitations
+
+A [Serverless](https://serverless.readme.io) setup is missing at the moment, but the _./src/index.js_ can simply be copied
+into the Lambda function's code window of the AWS console. 
+
+## Useful Alexa Links
 
 - https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speech-synthesis-markup-language-ssml-reference
 - http://alexa.design/slides
 - http://bit.ly/alexahighlow
 - http://bit.ly/alexa-cookbook
 - http://bit.ly/alexa-iot
+
+## Legal disclaimer
+
+This project and its creator is not affiliated with Chuck Norris, any motion picture corporation, any television
+corporation, parent, or affiliate corporation. All motion pictures, products, and brands mentioned in this README
+are the respective trademarks and copyrights of their owners. All content provided by this project is intended for
+humorous entertainment (satire) purposes only. The content provided by this project is not necessarily true and
+should not be regarded as truth!
--- src/index.js
+++ src/index.js
@@ -1,67 +1,158 @@
-'use strict';
+/* eslint-disable import/no-commonjs, no-console, object-shorthand, prefer-destructuring, func-names */
 const Alexa = require('alexa-sdk');
 const https = require('https');
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: var APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = undefined;
+/**
+ * Configured application ID.
+ * @type {string}
+ * @private
+ */
+const APP_ID = process.env.APP_ID;
 
-const SKILL_NAME = 'Chuck Norris Facts';
-const GET_FACT_MESSAGE = 'Here\'s your fact: ';
-const HELP_MESSAGE = 'You can say tell me a Chuck Norris fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
+/**
+ * The skill name.
+ * @type {string}
+ * @private
+ */
+const SKILL_NAME = 'Chuck Norris';
 
-exports.handler = function (event, context) {
-  const alexa = Alexa.handler(event, context);
-  alexa.APP_ID = APP_ID;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
+/**
+ * Prefix for joke answer.
+ * @type {string}
+ * @private
+ */
+const GET_FACT_MESSAGE = 'Here\'s the joke: ';
 
-const handlers = {
-  'LaunchRequest': function () {
-    this.emit('GetNewFactIntent');
+/**
+ * I18n resources.
+ * @type {Object}
+ * @private
+ */
+const I18N = {
+  'en-GB': {
+    translation: {
+      HELP_MESSAGE: 'You can say tell me a Chuck Norris joke, or, you can say exit... What can I help you with?',
+      HELP_REPROMPT: 'What can I help you with?',
+      STOP_MESSAGE: 'Goodbye!',
+    }
   },
-  'GetNewChuckNorrisFactIntent': function () {
-    httpGet(undefined, (myResult) => {
-      console.log('received : ' + myResult);
-      console.log('received value: ' + JSON.parse(myResult).value);
-      const speechOutput = GET_FACT_MESSAGE + JSON.parse(myResult).value;
-      this.emit(':tellWithCard', speechOutput, SKILL_NAME);
-      //this.emit(':tell', myResult.value);
-    });
+  'en-US': {
+    translation: {
+      HELP_MESSAGE: 'You can say tell me a Chuck Norris joke, or, you can say exit... What can I help you with?',
+      HELP_REPROMPT: 'What can I help you with?',
+      STOP_MESSAGE: 'Goodbye!',
+    }
   },
-  'GetNewChuckNorrisFactAboutIntent': function () {
-    const factType = this.event.request.intent.slots.factType.value;
-    httpGet(factType, (myResult) => {
-      console.log('received : ' + myResult);
-      const speechOutput = GET_FACT_MESSAGE + JSON.parse(myResult).value;
-      this.emit(':tellWithCard', speechOutput, SKILL_NAME);
-      //this.emit(':tell', myResult.value);
-    });
-  },
-  'AMAZON.HelpIntent': function () {
-    this.emit(':ask', HELP_MESSAGE, HELP_REPROMPT);
-  },
-  'AMAZON.CancelIntent': function () {
-    this.emit(':tell', STOP_MESSAGE);
-  },
-  'AMAZON.StopIntent': function () {
-    this.emit(':tell', STOP_MESSAGE);
+  'de-DE': {
+    translation: {
+      HELP_MESSAGE: 'Du kannst mir sagen frage Chuck Norris nach einem Witz, oder Du kannst Stop sagen... ' +
+        'Wie kann ich Dir helfen?',
+      HELP_REPROMPT: 'Wie kann ich Dir helfen?',
+      STOP_MESSAGE: 'Auf Wiedersehen!',
+    }
   }
 };
 
-// https is a default part of Node.JS.  Read the developer doc:  https://nodejs.org/api/https.html
-// try other APIs such as the current bitcoin price : https://btc-e.com/api/2/btc_usd/ticker  returns ticker.last
+/**
+ * A locale pattern used to match the language part.
+ * @type {RegExp}
+ * @private
+ */
+const LOCALE_PATTERN = /^([a-z]{2})(?:[-_]([A-Z]{2})?)$/;
 
-function httpGet(category, callback) {
-  console.log('got to the http function');
-  // GET is a web service request that is fully defined by a URL string
-  // Try GET in your browser:
-  // http://numbersapi.com/42
+/**
+ * Microsoft Translator API function.
+ *
+ * @param {string} speechOutput - The speech to translate.
+ * @param {string} language     - ISO language code.
+ * @param {Function} callback   - Done callback which takes the API result as argument.
+ */
+function translate(speechOutput, language, callback) {
+  console.log('MS translate function');
 
-  // Update these options with the details of the web service you would like to call
+  /**
+   * Clean the speech output from SSML tags before translation!
+   * @type {string}
+   * @private
+   */
+  const strippedSpeechOutput = speechOutput
+    .replace('<speak>', '')
+    .replace('</speak>', '')
+    .trim();
+
+  console.log('strippedSpeechOutput: ' + strippedSpeechOutput);
+
+  /**
+   * Request options for MS Translation API.
+   * @type {Object}
+   * @private
+   */
+  const options = {
+    host: 'api.microsofttranslator.com',
+    protocol: 'https:',
+    // eslint-disable-next-line max-len
+    path: `/V2/Http.svc/Translate?text=${encodeURIComponent(strippedSpeechOutput)}&from=en&to=${language}&contentType=text/plain`,
+    method: 'GET',
+    headers: { 'Ocp-Apim-Subscription-Key': process.env.MS_OCP_APIM_SUBSCRIPTION_KEY },
+  };
+
+  /**
+   * Request translation.
+   * @private
+   */
+  const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    if (res.statusCode !== 200) {
+      console.error(`An error occured while translating the Chuck Norris joke, error code: ${res.statusCode}.`);
+      callback(`An error occurred while translating the Chuck Norris joke, error code: ${res.statusCode}.`);
+      return;
+    }
+
+    const contentType = res.headers['content-type'];
+    if (!/^application\/xml/.test(contentType)) {
+      console.error(`An error occured while translating the Chuck Norris joke, error code: ${res.statusCode}. ` +
+        `Invalid content-type. Expected application/json but received ${contentType}`);
+      callback('An error occurred while translating the Chuck Norris joke. Invalid content-type.');
+    }
+
+    res.setEncoding('utf8');
+    let result = '';
+
+    res.on('data', (chunk) => {
+      result += chunk;
+    });
+
+    req.on('error', (err) => {
+      console.error('An error occurred while translating the Chuck Norris joke: ' + err);
+      callback('Es gab einen Fehler bei der Übersetzung.');
+    });
+
+    res.on('end', () => {
+      console.log(`TRANSLATED to ${language} (XML): ${result}`);
+      result = result
+        .replace('<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">', '')
+        .replace('</string>', '');
+      console.log(`TRANSLATED to ${language}: ${result}`);
+      callback(result);
+    });
+  });
+  req.end();
+}
+
+/**
+ * Gets a _random_ or _categorized_ joke from the Chuck Norris API.
+ *
+ * @param {string} [category] - A joke category to select.
+ * @param {Function} callback - Done callback which takes the API result as argument.
+ */
+function httpGetFromChuckNorris(category, callback) {
+  console.log('httpGetFromChuckNorris function');
+
+  /**
+   * Request options for Chuck Norris API.
+   * @type {Object}
+   * @private
+   */
   const options = {
     host: 'api.chucknorris.io',
     protocol: 'https:',
@@ -69,12 +160,26 @@ function httpGet(category, callback) {
     method: 'GET',
   };
 
+  /**
+   * Request joke.
+   * @private
+   */
   const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    if (res.statusCode !== 200) {
+      callback(`An error occurred while getting the Chuck Norris joke, error code: ${res.statusCode}.`);
+      return;
+    }
     res.setEncoding('utf8');
-    let returnData = '';
+    let result = '';
 
     res.on('data', (chunk) => {
-      returnData = returnData + chunk;
+      result += chunk;
+    });
+
+    req.on('error', (err) => {
+      console.error('An error occurred while getting the Chuck Norris joke: ' + err);
+      callback('An error occurred while getting the Chuck Norris joke.');
     });
 
     res.on('end', () => {
@@ -82,9 +187,94 @@ function httpGet(category, callback) {
       // We can see it in the log output via:
       // console.log(JSON.stringify(returnData))
       // we may need to parse through it to extract the needed data
-      console.log(returnData);
-      callback(returnData);  // this will execute whatever function the caller defined, with one argument
+      console.log(result);
+      callback(result);
     });
   });
   req.end();
 }
+
+/**
+ * All intent handlers used in the Lambda skill.
+ * @type {Object}
+ * @private
+ */
+const handlers = {
+  LaunchRequest: function () {
+    // Use default!
+    this.emit('GetNewChuckNorrisFactIntent');
+  },
+  GetNewChuckNorrisFactIntent: function () {
+    // Needed to detect language.
+    const locale = this.event.request.locale;
+    console.log('LOCALE', locale);
+
+    httpGetFromChuckNorris(undefined, (myResult) => {
+      console.log('received : ' + myResult);
+      console.log('received value: ' + JSON.parse(myResult).value);
+      const speechOutput = GET_FACT_MESSAGE + JSON.parse(myResult).value;
+
+      const matches = LOCALE_PATTERN.exec(locale);
+      // The matches looks something like this:
+      // ["en-US", "en", "US", index: 0, input: "en-US"]
+      const language = matches[1];
+      console.log('LANGUAGE: ' + language);
+      if (language === 'en') { // do not translate from 'en' to 'en'!
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME);
+      } else {
+        translate(speechOutput, language, (translatedSpeechOutput) => {
+          this.emit(':tellWithCard', translatedSpeechOutput, SKILL_NAME);
+        });
+      }
+    });
+  },
+  GetNewChuckNorrisFactAboutIntent: function () {
+    const factType = this.event.request.intent.slots.factType.value;
+    // Needed to detect language.
+    const locale = this.event.request.locale;
+    console.log('LOCALE', locale);
+
+    httpGetFromChuckNorris(factType, (myResult) => {
+      console.log('received : ' + myResult);
+      const speechOutput = GET_FACT_MESSAGE + JSON.parse(myResult).value;
+      const matches = LOCALE_PATTERN.exec(locale);
+      // The matches looks something like this:
+      // ["en-US", "en", "US", index: 0, input: "en-US"]
+      const language = matches[1];
+      console.log('LANGUAGE: ' + language);
+      if (language === 'en') { // do not translate from 'en' to 'en'!
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME);
+      } else {
+        translate(speechOutput, language, (translatedSpeechOutput) => {
+          this.emit(':tellWithCard', translatedSpeechOutput, SKILL_NAME);
+        });
+      }
+    });
+  },
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', this.t('HELP_MESSAGE'), this.t('HELP_REPROMPT'));
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  }
+};
+
+/**
+ * Lambda entry point delegating to the _alexa-sdk_.
+ *
+ * @param {Object} event   - The Lambda event information.
+ * @param {Object} context - The Lambda context information.
+ * @public
+ */
+exports.handler = function (event, context) {
+  const alexa = Alexa.handler(event, context);
+  alexa.appId = APP_ID;
+  // To enable string internationalization (i18n) features, set a resources object.
+  alexa.resources = I18N;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+/* eslint-enable import/no-commonjs, no-console, object-shorthand, prefer-destructuring, func-names */
